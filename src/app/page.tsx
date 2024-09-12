f8ccce9e141b6b1bccd0f8fe2ef48577430def2a
Phaneendra <rayudu13180@gmail.com>
2024-09-12 16:35:13 +0530

    Added Nav bar to User Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import identityContext from "@/utils/session";
-import {redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 import React from "react";
-import {sendGetRequest} from "@/utils/api";
-import {ApiResponsePage, IExamCentre, IExamDate, IFileType} from "@/types/types";
+import { sendGetRequest } from "@/utils/api";
+import { ApiResponsePage, IExamCentre, IExamDate, IFileType } from "@/types/types";
 import UserMainSection from "@/components/user-main-section";
 import UserHeader from "@/components/user-header";
+import { handleLogout } from "@/components/admin/exam-centre-container";
+import { faBars, faUser, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-const API_URL = process.env.API_URL as string
-const ADMIN_ROLE_CODE = process.env.ADMIN_ROLE_CODE as string
+const API_URL = process.env.API_URL as string;
+const ADMIN_ROLE_CODE = process.env.ADMIN_ROLE_CODE as string;
 if (!API_URL) {
     throw new Error('API_URL environment variable is not set');
 }
@@ -23,20 +25,20 @@ function isEmpty(fileTypes: IFileType[]) {
 export default async function Page() {
     const idContext = identityContext();
     if (!idContext.authenticated) {
-        redirect("/login")
+        redirect("/login");
     }
-    // if admin tries to access home apiResponsePage(/) of user then redirect to /admin as admin has different home apiResponsePage
+    // Redirect admin to /admin
     if (idContext.tokenClaims?.permissions.includes(ADMIN_ROLE_CODE)) {
-        redirect("/admin")
+        redirect("/admin");
     }
 
-    const examCentreCode = idContext.tokenClaims?.sub as string
+    const examCentreCode = idContext.tokenClaims?.sub as string;
     const token = idContext.token as string;
 
     const examCentreUrl = `${API_URL}/exam-centres/query?code=${examCentreCode}`;
     const fileTypesUrl = `${API_URL}/file-types`;
 
-    // fetch currently
+    // Fetch data
     const [examCentreApiRes, fileTypesApiRes] = await Promise.all([
         sendGetRequest(examCentreUrl, token),
         sendGetRequest(fileTypesUrl, token),
@@ -58,7 +60,7 @@ export default async function Page() {
     const examCentre: IExamCentre = examCentreApiResponsePage.items[0] as IExamCentre;
 
     const examDateUrl = `${API_URL}/exam-dates/query?examCentreId=${examCentre.id}`;
-    const examDateApiResponse = await sendGetRequest(examDateUrl, token)
+    const examDateApiResponse = await sendGetRequest(examDateUrl, token);
     if (!examDateApiResponse.status) {
         console.log(`error: status=${examDateApiResponse.status}, message=${examDateApiResponse.message}`);
         throw new Error("Error fetching exam dates.");
@@ -67,21 +69,43 @@ export default async function Page() {
     const examDates: IExamDate[] = examDateApiResponsePage.items as IExamDate[];
 
     return (
-        <main className="flex justify-center font-[sans-serif]">
-            <div className="flex h-screen w-full flex-col items-center gap-2 bg-gray-50 shadow-lg sm:w-[80vw]">
-                <UserHeader examCentre={examCentre}/>
-                {
-                    examDates.length === 0 ?
-                        <div className="text-large font-bold text-center justify-center items-center m-auto">
-                            <h2>No exam found!</h2>
-                            <h2> Kindly contact the admin to add exam.</h2>
-                        </div>
-                        :
-                        <UserMainSection examCentre={examCentre}
-                                         fileTypes={fileTypesApiRes.data as IFileType[]}
-                                         examDates={examDates.sort((a, b) => a.date.toLowerCase().localeCompare(b.date.toLowerCase()))}/>
-                }
+        <div className="min-h-screen flex flex-col items-center bg-gray-50 ">
+            {/* Header */}
+            <header className="w-full bg-[#0056b3] p-4 text-white flex items-center justify-between sticky top-0 ">
+                <div className="flex items-center space-x-4">
+                    <h1 className="text-xl font-bold">EXAM BACKUP</h1>
+                </div>               
+            </header>
+
+            {/* Content Box */}
+            <div className="flex flex-col w-full bg-white shadow-lg font-bold rounded-lg p-4 mt-4" style={{ width: '80vw', height: '40vw' }}>
+
+                <UserHeader examCentre={examCentre} />
+                
+                {/* Main Content */}
+                {examDates.length === 0 ? (
+                    <div className="text-large font-bold text-center m-auto ">
+                        <h2>No exam found!</h2>
+                        <h2>Kindly contact the admin to add exam.</h2>
+                    </div>
+                ) : (
+                    <UserMainSection
+                        examCentre={examCentre}
+                        fileTypes={fileTypesApiRes.data as IFileType[]}
+                        examDates={examDates.sort((a, b) => a.date.toLowerCase().localeCompare(b.date.toLowerCase()))}
+                    />
+                )}
+                
+                {/* Logout Button */}
+                <div className="flex justify-center mt-8">
+                    <button
+                        className="bg-primary-700 hover:bg-primary-800 text-white border border-gray-300 rounded-lg focus:ring focus:outline-none focus:ring-primary-300 font-bold px-8 py-4 text-sm"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
+                </div>
             </div>
-        </main>
+        </div>
     );
-};
+}
