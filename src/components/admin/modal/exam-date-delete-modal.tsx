8d76a31a283494195917dadd870b2a7b5aae595a
Phaneendra <rayudu13180@gmail.com>
2024-09-12 17:22:36 +0530

    Updated exm-Date page CRUD operations

--- src/components/admin/modal/exam-date-delete-modal.tsx
+++ src/components/admin/modal/exam-date-delete-modal.tsx
@@ -45,7 +45,7 @@ const AddAndEditModal = ({
             {isLoading ? <Loading/> :
                 <div className="sm:w-[40vw] bg-gray-100 flex flex-col shadow-lg rounded-lg">
                     <div className="border-b-1 border-gray-500">
-                        <h2 className="text-center text-medium text-gray-900 p-2">
+                        <h2 className="text-center text-medium text-white font-bold bg-blue-500 rounded-lg p-2">
                             {title}
                         </h2>
                     </div>
@@ -56,16 +56,16 @@ const AddAndEditModal = ({
                             </div> :
                             <div className="flex flex-col justify-center items-center gap-3 p-2 mt-4">
                                 <label>Are you sure to delete <b>{type}</b>?</label>
-                                <p>Date: {date}</p>
+                                <b>Date: {date}</b>
                             </div>}
-                        <div className="flex justify-center gap-4 p-2 mt-8 text-white">
+                        <div className="flex justify-center gap-4 p-2 mt-8 text-white font-bold">
                             <button
                                 className={`sm:w-[20%] bg-green-500 py-2 px-4 rounded-md disabled:active:bg-green-500 active:bg-green-700`}
                                 onClick={handleSaveClick}>
                                 Delete
                             </button>
                             <button
-                                className={`sm:w-[20%] bg-yellow-500 py-2 px-4 rounded-md disabled:active:bg-yellow-500`}
+                                className={`sm:w-[20%] bg-red-500 py-2 px-4 rounded-md disabled:active:bg-red-500`}
                                 onClick={handleCancelClick}>
                                 Cancel
                             </button>
