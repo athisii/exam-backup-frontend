fa60f39e3cbc25418ab4d3c29d818097e54ca2d0
Phaneendra <rayudu13180@gmail.com>
2024-09-13 10:17:06 +0530

    Exam Date Page Realignment

--- src/components/admin/modal/exam-date-delete-modal.tsx
+++ src/components/admin/modal/exam-date-delete-modal.tsx
@@ -44,8 +44,8 @@ const AddAndEditModal = ({
         <div className="fixed inset-0 bg-white bg-opacity-50 backdrop-blur-md flex justify-center items-center">
             {isLoading ? <Loading/> :
                 <div className="sm:w-[40vw] bg-gray-100 flex flex-col shadow-lg rounded-lg">
-                    <div className="border-b-1 border-gray-500">
-                        <h2 className="text-center text-medium text-white font-bold bg-blue-500 rounded-lg p-2">
+                    <div className="border-b-1">
+                        <h2 className="text-center text-medium text-white font-bold bg-blue-500 rounded-md p-2">
                             {title}
                         </h2>
                     </div>
