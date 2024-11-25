5c90a71b58337923305ce17f7182bfd567d3ecce
athisii <daiho.ekhe@gmail.com>
2024-11-25 18:38:16 +0530

    make responsive nav bar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,76 +1,51 @@
 "use client";
 
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import NavLink from "@/components/admin/nav-link";
 
 
-// TODO: need to work on this
 export default function Layout({
                                    children,
                                }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [isMobileView, setIsMobileView] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    // Handle window resize
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobileView(window.innerWidth < 768);
-        };
-
-        // Initial check
-        handleResize();
-
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
     return (
-        <main className="flex flex-col w-full h-full">
-            {/* Header Part */}
-            <header className="w-full bg-blue-500 p-2 text-white flex items-center justify-between">
-                <div className="flex items-center space-x-4">
+        <main className="flex flex-col absolute w-full h-full">
+            <header className="w-full bg-blue-500 p-2 text-white">
+                <div className="flex items-center justify-between">
                     <h1 className="text-xl font-bold">EXAM BACKUP</h1>
-                    {/* Button for toggling sidebar, visible only on mobile */}
-                    {isMobileView && (
-                        <button onClick={toggleSidebar} className="text-white text-2xl mr-4">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <line x1="3" y1="12" x2="21" y2="12"/>
-                                <line x1="3" y1="6" x2="21" y2="6"/>
-                                <line x1="3" y1="18" x2="21" y2="18"/>
+                    <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white lg:hidden text-2xl mr-4">
+                        {isMenuOpen ?
+                            <svg className="h-6 w-6 stroke-white cursor-pointer" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 24 24"
+                                 strokeWidth="2"
+                                 strokeLinecap="round"
+                                 strokeLinejoin="round">
+                                <line x1="18" y1="6" x2="6" y2="18"/>
+                                <line x1="6" y1="6" x2="18" y2="18"/>
                             </svg>
-                        </button>
-                    )}
+                            :
+                            <svg className="h-6 w-6 stroke-white" viewBox="0 0 24 24"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M4 6h16M4 12h16M4 18h16"/>
+                            </svg>
+                        }
+                    </button>
+                </div>
+                <div
+                    className={`w-full items-center gap-6 font-semibold ${isMenuOpen ? "flex flex-col" : "hidden"}`}>
+                    <NavLink/>
                 </div>
             </header>
-            <div className="flex h-full">
-                {/* Sidebar */}
-                {isSidebarOpen && (
-                    <div
-                        className="bg-gray-50 text-black flex flex-col justify-start items-center sm:w-[15vw] sm:h-[100vh] shadow-lg p-4 space-y-4 font-bold">
-                        <NavLink/>
-                    </div>
-                )}
-
-                {/* Main Content */}
-                <div className="flex h-screen w-full flex-col items-center gap-1 bg-gray-50 shadow-lg sm:w-[82vw] py-4">
+            <div className="flex h-full justify-center">
+                <div
+                    className={`bg-gray-50 hidden text-black lg:flex flex-col justify-start items-center lg:w-[15vw] lg:h-[100vh] shadow-lg p-4 space-y-4 font-bold1`}>
+                    <NavLink/>
+                </div>
+                <div className="flex h-screen w-full flex-col items-center gap-1 bg-gray-50 shadow-lg lg:w-[82vw] py-1">
                     {children}
                 </div>
             </div>
